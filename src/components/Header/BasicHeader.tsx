b24ccc00186d7839cf51e47c43b6b6b401f29553
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-06-15 11:46:59 +0900

    Feat: BasicHeader 마크업 완성

--- src/components/Header/BasicHeader.tsx
+++ src/components/Header/BasicHeader.tsx
@@ -1,7 +1,23 @@
+import { Menus, Search, TbagTitle } from "@/components/icons";
+
 const BasicHeader = () => {
   return (
-    <header>
-      <h1>Basic Header</h1>
+    <header
+      className={"w-full h-[82px] flex items-center px-6 justify-between"}>
+      <TbagTitle
+        width={80}
+        height={22}
+      />
+      <div className={"flex items-center gap-x-3.5"}>
+        <Search
+          width={32}
+          height={32}
+        />
+        <Menus
+          width={32}
+          height={32}
+        />
+      </div>
     </header>
   );
 };
