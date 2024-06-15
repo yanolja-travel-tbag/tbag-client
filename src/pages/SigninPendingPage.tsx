21974967c5788189c7ecd636df2b86a663e6b861
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-06-15 17:09:00 +0900

    Feat: 로그인시 authStore에 파라미터값 저장

--- src/pages/SigninPendingPage.tsx
+++ src/pages/SigninPendingPage.tsx
@@ -1,14 +1,19 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { useEffect } from "react";
+import authStore from "@/store/authStore.ts";
 
 const SigninPendingPage = () => {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
+  const { setUserId, setToken } = authStore();
   useEffect(() => {
     const isMember = searchParams.get("isRegistered") === "true";
     const acToken = searchParams.get("accessToken");
     const rfToken = searchParams.get("refreshToken");
     const userId = searchParams.get("userId");
+
+    userId && setUserId(userId);
+    acToken && rfToken && setToken(acToken, rfToken);
 
     if (isMember) {
       navigate("/");
