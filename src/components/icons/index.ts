68d65535b190819a6f1d9487e8324dced1e477a7
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-06-15 13:04:52 +0900

    Feat: 로그인 페이지 마크업 완성

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,4 +1,7 @@
+export { default as GoogleStart } from "./GoogleStart";
+export { default as KakaoStart } from "./KakaoStart";
 export { default as Menus } from "./Menus";
+export { default as NonMemberStart } from "./NonMemberStart";
 export { default as Search } from "./Search";
 export { default as TbagLogo } from "./TbagLogo";
 export { default as TbagTitle } from "./TbagTitle";
--- src/pages/SigninPage.tsx
+++ src/pages/SigninPage.tsx
@@ -1,7 +1,37 @@
+import {
+  GoogleStart,
+  KakaoStart,
+  NonMemberStart,
+  TbagTitle
+} from "@/components/icons";
+
 const SigninPage = () => {
   return (
-    <div>
-      <h1>Signin Page</h1>
+    <div
+      className={
+        "h-full flex flex-col items-center justify-center gap-y-[66px]"
+      }>
+      <div className={"flex flex-col items-center gap-y-4"}>
+        <TbagTitle
+          width={290}
+          height={90}
+        />
+        <h1 className={"text-[20px] font-semibold"}>시작하기</h1>
+      </div>
+      <div className={"flex flex-col items-center gap-y-2"}>
+        <KakaoStart
+          width={287}
+          height={39}
+        />
+        <GoogleStart
+          width={287}
+          height={39}
+        />
+        <NonMemberStart
+          width={287}
+          height={39}
+        />
+      </div>
     </div>
   );
 };
