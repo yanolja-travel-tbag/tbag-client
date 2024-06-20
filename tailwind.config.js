62d081b80b6f99fff6ee1e06ad062c3423bdedd0
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-06-21 08:46:49 +0900

    Chore: 컬러 팔레트 등록

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}"
   ],
   prefix: "",
   theme: {
@@ -13,25 +13,40 @@ module.exports = {
       center: true,
       padding: "2rem",
       screens: {
-        "2xl": "1400px",
-      },
+        "2xl": "1400px"
+      }
     },
     extend: {
+      colors: {
+        "main-primary": "#7FB185",
+        "main-secondary": "#D4E3A7",
+        "main-tertiary": "#609A79",
+        "point-high": "#E53F65",
+        "point-mid": "#FFC6D3",
+        "point-low": "#FFECF1",
+        "background-main": "#FFFCF7",
+        "background-section": "#FCF9EC",
+        "background-deep": "#F1F1F1",
+        "font-head": "#151515",
+        "font-body": "#434343",
+        "font-info": "#929292",
+        "font-disable": "#C4C4C4"
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "var(--radix-accordion-content-height)" }
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
+          to: { height: "0" }
+        }
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
-    },
+        "accordion-up": "accordion-up 0.2s ease-out"
+      }
+    }
   },
-  plugins: [require("tailwindcss-animate")],
-}
+  plugins: [require("tailwindcss-animate")]
+};
