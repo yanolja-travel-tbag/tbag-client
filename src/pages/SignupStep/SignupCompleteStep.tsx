a7b479f5166c4a00946ff622db92a96dc3387584
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-06-21 22:23:02 +0900

    Feat: 회원가입 완료 페이지 추가

--- src/pages/SignupStep/SignupCompleteStep.tsx
+++ src/pages/SignupStep/SignupCompleteStep.tsx
@@ -1,8 +1,32 @@
+import { TbagTitle } from "@/components/icons";
+import { Button } from "@/components/ui/button.tsx";
+import { useNavigate } from "react-router-dom";
+
 const SignupCompleteStep = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>회원가입 완료</h1>
-      <p>회원가입이 완료되었습니다.</p>
+    <div className={"flex flex-col items-center gap-[18px] mt-[164px]"}>
+      <TbagTitle
+        width={290}
+        height={90}
+      />
+      <div className={"flex flex-col items-center mb-[132px]"}>
+        <h1 className={"text-[16px] font-semibold"}>
+          {"회원 가입이 완료됐습니다!"}
+        </h1>
+        <h2 className={"text-[16px] font-semibold"}>
+          {"TBAG 회원이 되신 것을 환영합니다. "}
+        </h2>
+      </div>
+      <Button
+        variant={"ghost"}
+        className={
+          "w-[229px] h-[43px] rounded-[40px] bg-white border border-main-primary drop-shadow text-black"
+        }
+        onClick={() => navigate("/")}>
+        {"시작하기"}
+      </Button>
     </div>
   );
 };
