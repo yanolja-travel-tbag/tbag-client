da6ef0c2f7e844e378c7c0f6f3033cec5f0df841
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-06-21 22:22:42 +0900

    Feat: 뒤로가기 헤더에 뒤로가는 기능 추가

--- src/components/Header/BackHeader.tsx
+++ src/components/Header/BackHeader.tsx
@@ -1,6 +1,8 @@
 import { ArrowLeft } from "@/components/icons";
+import { useNavigate } from "react-router-dom";
 
 const BackHeader = () => {
+  const navigate = useNavigate();
   return (
     <header
       className={"w-full h-[46px] flex justify-start items-center px-[21px]"}>
@@ -8,7 +10,7 @@ const BackHeader = () => {
         width={12}
         height={12}
         className={"cursor-pointer"}
-        onClick={() => {}}
+        onClick={() => navigate(-1)}
       />
     </header>
   );
