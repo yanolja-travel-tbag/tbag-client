4ced7b12a3d4b165bb10067acaca970bf1db51b6
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-06-22 17:17:05 +0900

    Chore: 메인 페이지 지도 전체 꽉 차도록 변경

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -5,7 +5,7 @@ import NaverMaps from "@/components/Map/NaverMap.tsx";
 const MainPage = () => {
   return (
     <>
-      <MapContainer style={{ width: "100%", height: "900px" }}>
+      <MapContainer style={{ width: "100%", height: "100%" }}>
         <NaverMaps />
       </MapContainer>
       <MainBottomSheet />
