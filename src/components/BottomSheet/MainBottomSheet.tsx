e08b102e2cf3b5498cb0bb9cb18fd44c88ad589b
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-06-23 08:19:22 +0900

    Feat: 바텀시트 여행지 사진 영역 마크업 추가

--- src/components/BottomSheet/MainBottomSheet.tsx
+++ src/components/BottomSheet/MainBottomSheet.tsx
@@ -5,9 +5,24 @@ import {
 } from "@/components/ui/drawer.tsx";
 import { useState } from "react";
 import { Contents, History, Plane, User } from "@/components/icons";
+import authStore from "@/store/authStore.ts";
+import { useQuery } from "@tanstack/react-query";
+import getUserSelfData from "@/apis/getUserSelfData.ts";
+import Autoplay from "embla-carousel-autoplay";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem
+} from "@/components/ui/carousel.tsx";
 
 const MainBottomSheet = () => {
   const [snapPoint, setSnapPoint] = useState<number | string | null>("126px");
+  const { isRegistered } = authStore();
+  const { data } = useQuery({
+    queryKey: ["selfData"],
+    queryFn: getUserSelfData,
+    enabled: Boolean(isRegistered)
+  });
   return (
     <Drawer
       open={true}
@@ -23,7 +38,7 @@ const MainBottomSheet = () => {
           }
           onInteractOutside={() => setSnapPoint("126px")}
           onOpenAutoFocus={(event) => event.preventDefault()}>
-          <div className={"w-full h-full bg-white flex justify-center"}>
+          <div className={"bg-white flex flex-col items-center"}>
             <div className={"flex gap-[22px] mt-[22px]"}>
               <div
                 className={
@@ -74,6 +89,33 @@ const MainBottomSheet = () => {
                 </span>
               </div>
             </div>
+            <div
+              className={"h-0.5 border-2 border-background-deep w-full mt-5"}
+            />
+          </div>
+          <div className={"flex flex-col p-5 mt-[11px] gap-[10px]"}>
+            <h1 className={"text-[20px] font-semibold"}>
+              {isRegistered
+                ? `${data?.nickname}님을 위한 여행이에요`
+                : `로그인 후 이용해주세요`}
+            </h1>
+            <Carousel
+              opts={{
+                loop: true
+              }}
+              plugins={[Autoplay({ delay: 2000, stopOnMouseEnter: true })]}>
+              <CarouselContent>
+                {Array.from({ length: 5 }).map((_, index) => (
+                  <CarouselItem key={index}>
+                    <div
+                      className={
+                        "w-full h-[180px] bg-background-deep rounded-[8px] text-black flex items-center justify-center"
+                      }>{`여행지 사진-${index}`}</div>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+            </Carousel>
+            <h2>{"푸른 바다의 전설, 제주도로 떠나요"}</h2>
           </div>
         </DrawerContent>
       </DrawerPortal>
