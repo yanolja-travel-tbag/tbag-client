2b4d6bff2d6a9bb1c5802f146a8c3b1190f785a1
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-07-02 23:35:51 +0900

    Chore: 배우명으로 작품검색 api 엔드포인트 변경 대응

--- src/apis/getSearchedWorksByActor.ts
+++ src/apis/getSearchedWorksByActor.ts
@@ -4,7 +4,7 @@ import { SearchedWorksByActorResponse } from "@/apis/types.ts";
 const getSearchedWorksByActor = async (keyword: string) => {
   try {
     const response = await baseInstance.get<SearchedWorksByActorResponse>(
-      "/public/search/actor?keyword=" + keyword
+      "/public/actor/search?keyword=" + keyword
     );
     return response.data;
   } catch (error) {
