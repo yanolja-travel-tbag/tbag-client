4ceaf1db69238f7bef43d9462a24c29c21df8aef
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-07-23 08:27:13 +0900

    Chore: 푸터 국제화 및 링크 삽입

--- src/components/Footer/BasicFooter.tsx
+++ src/components/Footer/BasicFooter.tsx
@@ -1,4 +1,12 @@
+import { useI18n } from "@/hooks/useI18n.ts";
+
+const TERMS_URL =
+  "https://thirsty-wandflower-dee.notion.site/cacaf0f5bdd8499db298c88dabb1f750";
+const PRIVACY_POLICY_URL =
+  "https://thirsty-wandflower-dee.notion.site/7a6a556b0ce8478ca66a18477b2f537a";
+
 const BasicFooter = () => {
+  const t = useI18n();
   return (
     <footer
       className={
@@ -8,7 +16,11 @@ const BasicFooter = () => {
         className={
           "flex flex-col gap-[13px] text-center text-[12px] text-font-info"
         }>
-        <span>고객센터 ｜ 이용약관 ｜ 개인정보처리방침</span>
+        <span>
+          {t("footer.customerService")} ｜{" "}
+          <a href={TERMS_URL}>{t("footer.terms")}</a> ｜
+          <a href={PRIVACY_POLICY_URL}>{t("footer.privacy")}</a>
+        </span>
         <span>All rights reserved @TravelYoung · TBAG</span>
         <span>dev ver 1.1</span>
       </p>
